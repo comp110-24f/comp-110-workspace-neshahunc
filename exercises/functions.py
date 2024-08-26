4c58be86bed61da275687e5068ed845923f540be
Neil Shah <neshah@unc.edu>
2024-08-26 16:06:15 -0400

    Worked on hello world file

---
+++ exercises/functions.py
@@ -0,0 +1,12 @@
+"""Practice with functions"""
+
+
+# Define a function
+def sum(num1: int, num2: int) -> int:
+    """Returns sum of 2 numbers"""
+    return num1 + num2
+
+
+# Call the function
+
+print(sum(num1=1, num2=3))
